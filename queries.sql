d9ce0c972dad948b0c264009ea90937771127e23
redvg <redvg17@gmail.com>
2018-08-03 20:35:37 +0200

    query 2

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 /*
-    1. yields commits (paths) for given author and date:
+    1. yields paths affected by commit for given author and date:
     email| path | date
     const | different | const
     UNNEST is applied to a column which has repeated records, and flattens it
@@ -21,3 +21,21 @@ WHERE
   FROM
     author.date)=2016
 LIMIT 10
+
+
+/*
+    2. yields first path affected by commit for given author and date
+*/
+
+SELECT
+  author.email,
+  difference[OFFSET(0)].new_path AS path,
+  author.date
+FROM
+  `bigquery-public-data.github_repos.commits`
+WHERE
+  EXTRACT(YEAR
+  FROM
+    author.date)=2016
+LIMIT
+  10
