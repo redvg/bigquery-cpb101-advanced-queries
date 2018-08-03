ccb3ed72f40f09c1de693b17afe2721d7c133fe5
redvg <redvg17@gmail.com>
2018-08-03 20:43:33 +0200

    query 4

--- queries.sql
+++ queries.sql
@@ -40,7 +40,7 @@ LIMIT
   10
 
 /*
-    3. extract file extensions of all comits by author and date
+    3. extract file extensions of all commits by author and date
 */
 
 SELECT
@@ -57,3 +57,39 @@ WHERE
     author.date)=2016
 LIMIT
   10
+
+/*
+    4. group by language and list in descending order of the number of commits
+    Also filters the languages which consist purely of letters and have a
+    length that is fewer than 8 characters
+*/
+
+WITH
+  commits AS (
+  SELECT
+    author.email,
+    LOWER(REGEXP_EXTRACT(diff.new_path, r'\.([^\./\(~_ \- #]*)$')) lang,
+    diff.new_path AS path,
+    author.date
+  FROM
+    `bigquery-public-data.github_repos.commits`,
+    UNNEST(difference) diff
+  WHERE
+    EXTRACT(YEAR
+    FROM
+      author.date)=2016 )
+SELECT
+  lang,
+  COUNT(path) AS numcommits
+FROM
+  commits
+WHERE
+  LENGTH(lang) < 8
+  AND lang IS NOT NULL
+  AND REGEXP_CONTAINS(lang, '[a-zA-Z]')
+GROUP BY
+  lang
+HAVING
+  numcommits > 100
+ORDER BY
+  numcommits DESC
