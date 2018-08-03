9b877623f521dc46517dee6218cb275ed65725ef
redvg <redvg17@gmail.com>
2018-08-03 20:23:54 +0200

    authored commits

--- queries.sql
+++ queries.sql
@@ -1,1 +1,18 @@
+/*
+    1. yields authored commits:
+    email, path, date
+    UNNEST is used as path and email are nested props of parent object 
+*/
 
+SELECT
+  author.email,
+  diff.new_path AS path,
+  author.date
+FROM
+  `bigquery-public-data.github_repos.commits`,
+  UNNEST(difference) diff
+WHERE
+  EXTRACT(YEAR
+  FROM
+    author.date)=2016
+LIMIT 10
